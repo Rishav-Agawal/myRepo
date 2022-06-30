b6d79fbe939d61e7f794b967c37277396a867476
Reshu-Agarwal-au17/Rishav Agarwal <rishav2510246@gmail.com>
2022-06-30 15:56:42 +0530

    Marathon Day -1

---
+++ HC/01/01ass.js
@@ -0,0 +1,19 @@
+//1. Explain the use of JavaScript ( or What you can do using a JavaScript)
+/*Ans. Initally Javascript was made for client side and only run in browser.
+but now it was evolved a way lot we can use it for client Side, server Side, blockchain, cloud computing and many more there are n number of things we can do with javascript*/
+
+//2. What is the difference between client-side and server-side?
+/*Ans.Clinet side means the place which user can see and user can iteract.
+Server side means the process run on server basically it is known as backend where there is zero user interaction server side takes all the data and process it*/
+
+//3.What is Nodejs?
+//Ans. There are various types of engine whuch were embeded in browsers for running javascript. One popular engine named as v8 engine which was used in chrome was taken out and rap up around some process and named it as node js . node js helps us to run javascript code outside the browser. It helps javascript to run in terminal
+
+//4. Explain Scope in JavaScript
+
+//5. JavaScript is asynchronous or synchronous.
+
+//6. JavaScript is Single-threaded or Multi-threaded
+
+//7. Explain DOM in your own word.
+//Ans. Let us assume a Html as a tree where the roots are html tag , the trunk is body tag and there n number of branches the branches are like heading tag, pargraph tag, strong tag, etc.... The whole tree is known as DOM(Document Object Model)
---
+++ HC/01/01helloRishav.js
@@ -0,0 +1,8 @@
+console.log("Hello Rishav Agarwal");
+
+//Variable Decleration
+let username = "Rishav Agarwal";
+username = "Facebook";
+
+let userMobile = 9348003628;
+let isLoggedIn = true;
