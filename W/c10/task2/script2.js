f8ae1dd0bf221faeb26254ed8789b231c5c350ce
Reshu-Agarwal-au17/Rishav Agarwal <rishav2510246@gmail.com>
2022-07-05 00:14:48 +0530

    chapter 10

---
+++ W/c10/task2/script2.js
@@ -0,0 +1,19 @@
+function my() {
+  let x = document.getElementById("one");
+  x.innerHTML = "My";
+}
+
+function name_() {
+  let x = document.getElementById("two");
+  x.innerHTML = "Name";
+}
+
+function is() {
+  let x = document.getElementById("three");
+  x.innerHTML = "Is";
+}
+
+function rishav() {
+  let x = document.getElementById("four");
+  x.innerHTML = "Rishav";
+}
