973c40d2b9d51df82fae7eccb8afb4680c50f5ca
Reshu-Agarwal-au17/Rishav Agarwal <rishav2510246@gmail.com>
2022-07-08 17:17:43 +0530

    continuation of ch12 part 1

---
+++ W/c12/p1/script.js
@@ -0,0 +1,23 @@
+let x = document.getElementById("result");
+let y = document.getElementById("brand-name");
+function color() {
+  x.style.color = "red";
+}
+
+function fontFamily() {
+  x.style.fontFamily = "calibri";
+}
+
+function fontWeight() {
+  x.style.fontWeight = "lighter";
+}
+
+function clr() {
+  y.style.backgroundColor = "yellow";
+}
+
+function image1() {
+  y.style.backgroundImage =
+    "url(./images/photo-1504805572947-34fad45aed93.jpg)";
+  x.style.color = "white";
+}
