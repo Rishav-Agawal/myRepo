d420492f8a74af4bf4a08ae4f9ffaf5cd1751443
Rishav <rishav2510246@gmail.com>
2022-09-02 13:17:15 +0530

    Javascript

---
+++ HC/MERN/js/02basics/01Variables.js
@@ -0,0 +1,16 @@
+var fullName = "Rishav Agarwal";
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+console.log(loggedCount);
+
+var paymentMode;
+
+console.log(paymentMode)
+
+paymentMode = "Credit Card";
+console.log(paymentMode)
